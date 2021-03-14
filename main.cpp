cbaa1ea102fa0ba203853bcfe5dac2806d9866ac
AlexseiT <alex.todorov@mail.ru>
2021-03-14 14:59:18 +0700

    New code, trinagle

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
-const double pi = 3.141592653589793238463;
+
 enum Figures {
-	Circle, Trinagle, Polygon, Error
+	Circle, Trinagle, Error
 };
 
-struct circle {
-	float Point;
-	float Point1;
+struct circle
+{
+	float point[2];
 	float Number;
 };
+
+struct triangle
+{
+	float point1[2];
+	float point2[2];
+	float point3[2];
+	float point4[2];
+};
+
 
 string word_tolower(string word)
 {
@@ -21,11 +31,19 @@ string word_tolower(string word)
 	return word;
 }
 
-void Сircle_func(float Number) {
-	float perimetr = 2 * pi * Number;
-	float area = pi * Number * Number;
-	cout << "Perimetr: " << perimetr << endl;
-	cout << "area: " << area << endl;
+void Circle_param(circle Object_circle)
+{
+	float perimetr = 2 * 3.14 * Object_circle.Number;
+	float area = 3.14 * Object_circle.Number * Object_circle.Number;
+
+	cout << " perimetr = " << perimetr << endl;
+	cout << " area = " << area << endl;
+}
+
+void Triangle_param(triangle Object_triangle)
+{
+	float area = 0.5 * abs((Object_triangle.point2[0] - Object_triangle.point1[0]) * (Object_triangle.point3[1] - Object_triangle.point1[1]) - (Object_triangle.point3[0] - Object_triangle.point1[0]) * (Object_triangle.point2[1] - Object_triangle.point1[1]));
+	cout << "area = " << area << endl;
 }
 
 Figures identify(string figure) {
@@ -38,48 +56,84 @@ Figures identify(string figure) {
 	figureName = "triangle";
 	if (figure == figureName) return Trinagle;
 
-	figureName = "polygon";
-	if (figure == figureName) return Polygon;
-
 	return Error;
 }
 
-int main() {
-	string ObjectName;
-	string ObjectString;
-	circle Object;
+void parse_circle()
+{
 
-	getline(cin, ObjectString);
+}
 
-	if (ObjectString.substr(0, 1) == " ") {
-		ObjectString.erase(0, ObjectString.find_first_not_of(" "));
+int main()
+{
+	string input, figureName;
+	size_t end;
+
+	getline(cin, input);
+
+	if (input.substr(0, 1) == " ") {
+		input.erase(0, input.find_first_not_of(" "));
 	}
-	if (ObjectString.substr(ObjectString.find("(") - 1, 1) == " ") {
-		ObjectString = ObjectString.erase(ObjectString.find(" "), ObjectString.find("(") - ObjectString.find(" "));
+	if (input.substr(input.find("(") - 1, 1) == " ") {
+		input = input.erase(input.find(" "), input.find("(") - input.find(" "));
 	}
-	ObjectName = ObjectString.substr(0, ObjectString.find("("));
+	figureName = input.substr(0, input.find("("));
 
 
-	switch (identify(ObjectName)) {
-			case Circle:
-			ObjectString = ObjectString.erase(0, 6);
-			Object.Point = stof(ObjectString.substr(1, ObjectString.find(" ")));
-			ObjectString = ObjectString.erase(0, ObjectString.find(" "));
-			Object.Point1 = stof(ObjectString.substr(1, ObjectString.find(",") - 1));
-			ObjectString = ObjectString.erase(0, ObjectString.find(",") + 1);
-			Object.Number = stof(ObjectString.substr(1, ObjectString.find(")") - 1));
-			cout << "Point = " << Object.Point << endl;
-			cout << "Point1 = " << Object.Point1 << endl;
-			cout << "Number = " << Object.Number << endl;
-			Сircle_func(Object.Number);
-			break;
-		case Trinagle:
-			cout << "triangle" << endl;
-			break;
-		case Polygon:
-			cout << "polygon" << endl;
-			break;
-		case Error:
-			cout << "Error: the figure was not found";
+	switch (identify(figureName)) {
+	case Circle:
+		circle Object_circle;
+		input = input.erase(0, 6);
+		cout << input << endl;
+		if (input[0] == '(') {
+			input.erase(0, 1);
+			Object_circle.point[0] = stod(input, &end);
+			input = input.erase(0, end);
+			Object_circle.point[1] = stod(input, &end);
+			input = input.erase(0, end + 1);
+			Object_circle.Number = stod(input, &end);
+			cout << "Figure: circle" << endl;
+			cout << "Point =  " << Object_circle.point[0] << ' ' << Object_circle.point[1] << endl;
+			cout << "Number = " << Object_circle.Number << endl;
+			Circle_param(Object_circle);
+		}
+		break;
+	case Trinagle:
+		triangle Object_triangle;
+		float points[8];
+		input = input.erase(0, 8);
+		if ((input[0] == '(') && (input[1] == '(')) {
+			input = input.erase(0, 2);
+			replace(input.begin(), input.end(), ',', ' ');
+
+			Object_triangle.point1[0] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point1[1] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point2[0] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point2[1] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point3[0] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point3[1] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point4[0] = stod(input, &end);
+			input.erase(0, end);
+			Object_triangle.point4[1] = stod(input, &end);
+			input.erase(0, end);
+
+		}
+		cout << "Figure: triangle" << endl;
+		cout << "Point 1:" << Object_triangle.point1[0] << ' ' << Object_triangle.point1[1] << endl;
+		cout << "Point 2:" << Object_triangle.point2[0] << ' ' << Object_triangle.point2[1] << endl;
+		cout << "Point 3:" << Object_triangle.point3[0] << ' ' << Object_triangle.point3[1] << endl;
+		cout << "Point 4:" << Object_triangle.point4[0] << ' ' << Object_triangle.point4[1] << endl;
+		Triangle_param(Object_triangle);
+		break;
+
+	case Error:
+		cout << "Error: figure not found";
 	}
 }
+
