1bc9f0dc4cf12cc59d55527bd834539ff29fe6f8
AlexseiT <alex.todorov@mail.ru>
2021-03-21 14:59:40 +0700

    Fix .h files

--- func.h
+++ func.h
@@ -1,3 +1,4 @@
 #pragma once
+#include "parsing.h"
 void Circle_math(circle Object_circle);
 void Triangle_math(triangle Object_triangle);
--- parsing.h
+++ parsing.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "func.h"
 void parse_circle(string input, circle* Object_circle);
 void parse_triangle(string input, triangle* Object_triangle);
 string parse_name(string input);
