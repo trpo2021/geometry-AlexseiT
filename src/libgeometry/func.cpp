c4cf5fa2d5cecca59e3e098e732dc94a42a764c4
AlexseiT <alex.todorov@mail.ru>
2021-04-07 14:39:20 +0700

    fix triangle

--- src/libgeometry/func.cpp
+++ src/libgeometry/func.cpp
@@ -20,12 +20,12 @@ double distance_between_points(double x1, double x2, double y1, double y2)
 
 void find_triangle_param(mass_object triang, float* perimeter, float* area)
 {
-    double x1 = triang.obj_triagle.point_1[0];
-    double x2 = triang.obj_triagle.point_2[0];
-    double x3 = triang.obj_triagle.point_3[0];
-    double y1 = triang.obj_triagle.point_1[1];
-    double y2 = triang.obj_triagle.point_2[1];
-    double y3 = triang.obj_triagle.point_3[1];
+    double x1 = triang.obj_triangle.point_1[0];
+    double x2 = triang.obj_triangle.point_2[0];
+    double x3 = triang.obj_triangle.point_3[0];
+    double y1 = triang.obj_triangle.point_1[1];
+    double y2 = triang.obj_triangle.point_2[1];
+    double y3 = triang.obj_triangle.point_3[1];
 
     *perimeter = distance_between_points(x1, x2, y1, y2)
         + distance_between_points(x2, x3, y2, y3)
@@ -52,11 +52,11 @@ void print_circle(mass_object* circl, float* perimeter, float* area)
 void print_triangle(mass_object* triang, float* perimeter, float* area)
 {
     cout << triang->order_number << "."
-        << "triangle((" << triang->obj_triagle.point_1[0] << ' '
-        << triang->obj_triagle.point_1[1] << "," << triang->obj_triagle.point_2[0] << ' '
-        << triang->obj_triagle.point_2[1] << "," << triang->obj_triagle.point_3[0] << ' '
-        << triang->obj_triagle.point_3[1] << "," << triang->obj_triagle.point_4[0] << ' '
-        << triang->obj_triagle.point_4[1] << "))" << endl;
+        << "triangle((" << triang->obj_triangle.point_1[0] << ' '
+        << triang->obj_triangle.point_1[1] << "," << triang->obj_triangle.point_2[0] << ' '
+        << triang->obj_triangle.point_2[1] << "," << triang->obj_triangle.point_3[0] << ' '
+        << triang->obj_triangle.point_3[1] << "," << triang->obj_triangle.point_4[0] << ' '
+        << triang->obj_triangle.point_4[1] << "))" << endl;
 
     find_triangle_param(*triang, perimeter, area);
 
