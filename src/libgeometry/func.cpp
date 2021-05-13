20b6cac67785f69990e291b788e1dfd88ac3e27c
AlexseiT <alex.todorov@mail.ru>
2021-05-14 01:36:36 +0700

    fix clang circle_circle

--- src/libgeometry/func.cpp
+++ src/libgeometry/func.cpp
@@ -3,67 +3,75 @@
 #include <libgeometry/func.h>
 #include <string>
 using namespace std;
-double circle_p(double number) {
+double circle_p(double number)
+{
     double perim;
-    perim = 2 * M_PI * number;
+    perim = 2 * 3.14 * number;
     return perim;
 }
 
-double circle_area(double number) {
+double circle_area(double number)
+{
     double area;
-    area = M_PI * number * number;
+    area = 3.14 * number * number;
     return area;
 }
 
-double triangle_area(double x1, double x2, double x3, double y1, double y2,
-    double y3) {
+double
+triangle_area(double x1, double x2, double x3, double y1, double y2, double y3)
+{
     double area;
     area = abs(((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2);
     return area;
 }
 
-double triangle_p(double x1, double x2, double x3, double y1, double y2,
-    double y3) {
+double
+triangle_p(double x1, double x2, double x3, double y1, double y2, double y3)
+{
     double perim;
-    perim = distance_between_points(x1, x2, y1, y2) +
-        distance_between_points(x2, x3, y2, y3) +
-        distance_between_points(x3, x1, y3, y1);
+    perim = distance_between_points(x1, x2, y1, y2)
+            + distance_between_points(x2, x3, y2, y3)
+            + distance_between_points(x3, x1, y3, y1);
     return perim;
 }
 
-void find_circle_param(mass_object circl, float* perimeter, float* area) {
-    *perimeter = 2 * M_PI * circl.obj_circle.number;
+void find_circle_param(mass_object circl, float* perimeter, float* area)
+{
+    *perimeter = 2 * 3.14 * circl.obj_circle.number;
 
-    *area = M_PI * circl.obj_circle.number * circl.obj_circle.number;
+    *area = 3.14 * circl.obj_circle.number * circl.obj_circle.number;
 }
 
-double distance_between_points(double x1, double x2, double y1, double y2) {
+double distance_between_points(double x1, double x2, double y1, double y2)
+{
     double i = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 
     return i;
 }
 
-void find_triangle_param(mass_object triang, float* perimeter, float* area) {
+void find_triangle_param(mass_object triang, float* perimeter, float* area)
+{
     double x1 = triang.obj_triangle.point_1[0];
     double x2 = triang.obj_triangle.point_2[0];
     double x3 = triang.obj_triangle.point_3[0];
     double y1 = triang.obj_triangle.point_1[1];
     double y2 = triang.obj_triangle.point_2[1];
     double y3 = triang.obj_triangle.point_3[1];
-    *perimeter = distance_between_points(x1, x2, y1, y2) +
-        distance_between_points(x2, x3, y2, y3) +
-        distance_between_points(x3, x1, y3, y1);
+    *perimeter = distance_between_points(x1, x2, y1, y2)
+            + distance_between_points(x2, x3, y2, y3)
+            + distance_between_points(x3, x1, y3, y1);
 
     *area = abs(((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2);
 }
 
-void print_circle(mass_object* circl, float* perimeter, float* area) {
+void print_circle(mass_object* circl, float* perimeter, float* area)
+{
     cout << circl->order_number << "."
 
-        << "circle(" << circl->obj_circle.point[0] << ' '
-        << circl->obj_circle.point[1] << ", "
+         << "circle(" << circl->obj_circle.point[0] << ' '
+         << circl->obj_circle.point[1] << ", "
 
-        << circl->obj_circle.number << ")" << endl;
+         << circl->obj_circle.number << ")" << endl;
 
     find_circle_param(*circl, perimeter, area);
 
@@ -72,16 +80,17 @@ void print_circle(mass_object* circl, float* perimeter, float* area) {
     cout << "Area: " << *area << endl;
 }
 
-void print_triangle(mass_object* triang, float* perimeter, float* area) {
+void print_triangle(mass_object* triang, float* perimeter, float* area)
+{
     cout << triang->order_number << "."
-        << "triangle((" << triang->obj_triangle.point_1[0] << ' '
-        << triang->obj_triangle.point_1[1] << ","
-        << triang->obj_triangle.point_2[0] << ' '
-        << triang->obj_triangle.point_2[1] << ","
-        << triang->obj_triangle.point_3[0] << ' '
-        << triang->obj_triangle.point_3[1] << ","
-        << triang->obj_triangle.point_4[0] << ' '
-        << triang->obj_triangle.point_4[1] << "))" << endl;
+         << "triangle((" << triang->obj_triangle.point_1[0] << ' '
+         << triang->obj_triangle.point_1[1] << ","
+         << triang->obj_triangle.point_2[0] << ' '
+         << triang->obj_triangle.point_2[1] << ","
+         << triang->obj_triangle.point_3[0] << ' '
+         << triang->obj_triangle.point_3[1] << ","
+         << triang->obj_triangle.point_4[0] << ' '
+         << triang->obj_triangle.point_4[1] << "))" << endl;
 
     find_triangle_param(*triang, perimeter, area);
 
@@ -92,17 +101,21 @@ void print_triangle(mass_object* triang, float* perimeter, float* area) {
     cout << endl;
 }
 
-void circle_circle(circle* cir, int countC, int count) {
-    double Length;
-    for (int i = 0; i < countC; i++) {
-        if (i != count) {
-            Length = sqrt((cir[count].point[0] - cir[i].point[0]) *
-                (cir[count].point[0] - cir[i].point[0]) +
-                (cir[count].point[1] - cir[i].point[1]) *
-                (cir[count].point[1] - cir[i].point[1]));
-            if ((Length - 2 * cir[count].number) <= 0)
-                if ((Length - cir[count].number - cir[i].number) <= 0)
-                    cout << i + 1 << ". circle" << endl;
+
+void circle_circle(circle* cir, int countC, int count)
+{
+  double Length;
+  for (int i = 0; i < countC; i++) {
+    if (i != count) {
+      Length = sqrt((cir[count].point[0] - cir[i].point[0]) *
+                        (cir[count].point[0] - cir[i].point[0]) +
+                    (cir[count].point[1] - cir[i].point[1]) *
+                        (cir[count].point[1] - cir[i].point[1]));
+      if ((Length - 2 * cir[count].number) <= 0) {
+        if ((Length - cir[count].number - cir[i].number) <= 0) {
+          cout << i+1 << ". circle" << endl;
         }
+      }
     }
+  }
 }
